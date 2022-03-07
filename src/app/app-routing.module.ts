f6a79faa1c5ff0ecf807d81ef608dbd6be930029
newcombe <cn728016@dal.ca>
2022-03-07 17:17:09 -0400

    Implemented slide in animation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './core/components/page-not-found/pagenotfound.component';
+import { HomeComponent } from './features/home/home.component';
+  
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+  
+    //Wild Card Route for 404 request
+    { path: '**', pathMatch: 'full', 
+        component: PageNotFoundComponent },
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+    providers: []
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
