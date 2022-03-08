da57fb8021c9d151582c297bf6b48bcef00b51be
newcombe <cn728016@dal.ca>
2022-03-08 15:09:59 -0400

    Created bare bones resources component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './core/components/page-not-found/pagenotfound.component';
 import { AboutComponent } from './features/about/about.component';
 import { HomeComponent } from './features/home/home.component';
+import { ResourcesComponent } from './features/resources/resources.component';
   
 const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'about', component: AboutComponent},
+    { path: 'resources', component: ResourcesComponent},
   
     //Wild Card Route for 404 request
     { path: '**', pathMatch: 'full', 
---
+++ src/app/features/resources/resources.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'resources',
+  templateUrl: './resources.component.html',
+  styleUrls: ['./resources.component.scss'],
+})
+export class ResourcesComponent {
+}
