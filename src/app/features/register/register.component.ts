67b227272e7487afe30e7148fc4fc6d345721bc6
newcombe <cn728016@dal.ca>
2022-03-12 17:30:26 -0400

    Implemented password creation step. Added form validations.

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+import { LeanERErrorStateMatcher } from 'src/app/shared/utils/leaner-error-state-matcher';
+import { User } from 'src/db/db';
 
 @Component({
   selector: 'register',
@@ -10,10 +12,20 @@ export class RegisterComponent {
   personalInfoFormGroup!: FormGroup;
   contactInfoFormGroup!: FormGroup;
   emergContactFormGroup!: FormGroup;
+  passwordFormGroup!: FormGroup;
+
   minDate: Date;
   maxDate: Date;
 
-  sexes = [{ value: 0, viewValue: 'Male', }, { value: 1, viewValue: 'Female'}, { value: 2, viewValue: 'Prefer not to say', }]
+  hidePassword = true;
+  hideConfirmPassword = true;
+  matcher = new LeanERErrorStateMatcher();
+
+  sexes = [
+    { value: 0, viewValue: 'Male' },
+    { value: 1, viewValue: 'Female' },
+    { value: 2, viewValue: 'Prefer not to say' },
+  ];
 
   constructor(private _formBuilder: FormBuilder) {
     const currentYear = new Date().getFullYear();
@@ -40,6 +52,37 @@ export class RegisterComponent {
       emergNameCtrl: ['', Validators.required],
       emergPhoneNumberCtrl: ['', Validators.required],
     });
+    this.passwordFormGroup = this._formBuilder.group({
+      passwordCtrl: ['', Validators.required],
+      passwordConfirmCtrl: ['', Validators.required],
+    }, { validators: this.checkPasswords });
+  }
+
+  submitRegistration() {
+    // Populate user object
+    let personalInfoCtrls = this.personalInfoFormGroup.controls;
+    let contactInfoCtrls = this.contactInfoFormGroup.controls;
+    let emergContactCtrls = this.emergContactFormGroup.controls;
+    let user: User;
+    user = {
+      firstName: personalInfoCtrls['firstNameCtrl'].value,
+      lastName: personalInfoCtrls['lastNameCtrl'].value,
+      dob: personalInfoCtrls['dobCtrl'].value,
+      sex: personalInfoCtrls['sexCtrl'].value,
+      healthCardNumber: personalInfoCtrls['healthCardNumberCtrl'].value,
+      email: contactInfoCtrls['emailCtrl'].value,
+      phoneNumber: contactInfoCtrls['phoneNumberCtrl'].value,
+      addressLineOne: contactInfoCtrls['addressLineOneCtrl'].value,
+      addressLineTwo: contactInfoCtrls['addressLineTwoCtrl'].value,
+      postcode: contactInfoCtrls['postcodeCtrl'].value,
+      emergencyFullName: emergContactCtrls['emergNameCtrl'].value,
+      emergencyPhoneNumber: emergContactCtrls['emergPhoneNumberCtrl'].value,
+      password: this.passwordFormGroup.controls['passwordCtrl'].value,
+    };
+
+    // Add to DB
+
+    // Handle DB Response
   }
 
   getEmailErrorMessage() {
@@ -50,4 +93,15 @@ export class RegisterComponent {
     return '';
   }
 
+  checkPasswords: ValidatorFn = (group: AbstractControl):  ValidationErrors | null => { 
+    let pass = group.get('passwordCtrl');
+    if(pass) {
+      pass = pass.value;
+    }
+    let confirmPass = group.get('passwordConfirmCtrl');
+    if(confirmPass) {
+      confirmPass = confirmPass.value;
+    }
+    return pass === confirmPass ? null : { notSame: true };
+  }
 }
